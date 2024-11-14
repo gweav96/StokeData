188a5d06d1f6541bd3c67ce8a6972014c668e1ce
George Weaver <georgeweaver@Georges-MacBook-Pro-2.local>
2024-11-14 16:05:15 +0000

    Added all the summary plot code, and the basic data codes

--- xgdata.py
+++ xgdata.py
@@ -50,7 +50,7 @@ def get_xg(data, nmodels=5):
                 ev_r = dataset.loc[dataset['eventId'] == float(related)]
                 ev_r = ev_r.loc[ev_r['teamId'] == shots.iloc[i]['teamId']]
                 ev_r = ev_r.loc[ev_r['matchId'] == shots.iloc[i]['matchId']]
-                ev_r = ev_r['satisfiedEventsTypes']
+                ev_r = list(ev_r['satisfiedEventsTypes'].values)
                 shots['relatedevent'].iloc[i] = ev_r
         
         shots_training = shots[['x','y','isGoal','shotBodyType','situation', 'shotCounter',
